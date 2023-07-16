9b1d1cb3f148a1775e05ec8f886ebe14d8a6d671
SubirSam <saptarshi.subir@gmail.com>
2023-07-16 18:52:34 +0530

    My First ever kotlin G

---
+++ G1kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main(){
+    // var - mutable
+    // val - immutable/constant
+    // semicolon is optional
+    // can print value within string using $
+    // type can be inferred
+    var age:Int=29;
+    val pan:String="abcgdgghd11";
+    println("hello sir, welcome to the world of India Kotlin");
+    println("$age and $pan");
+
+    // array index starts from 0 here
+    // listOf - immutable
+    // mutableListOf - mutable
+
+    var immutableCountry= listOf<String>("India","Australia","UK","Germany","France","Russia","USA")
+    println(immutableCountry.get(5))
+    var country= mutableListOf<String>("India","Australia","UK","Germany","France","Russia","USA")
+    println(country[6])
+    country.set(6,"Japan")
+    println(country[6])
+
+}
